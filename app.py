0226940ed0d33ae5ece01d265b2c7b5936b529e3
nsylove97 <nsylove97@gmail.com>
2025-10-20 00:14:51 +0900

    bug fixed 01

--- app.py
+++ app.py
@@ -589,7 +589,7 @@ def main():
     credentials, all_user_info = fetch_all_users()
     authenticator = stauth.Authenticate(credentials, "ocp_cookie_v3", "auth_key_v3", 30)
 
-    name, authentication_status, username = authenticator.login('로그인', 'main')
+    name, authentication_status, username = authenticator.login('Login', 'main')
 
     if authentication_status:
         run_main_app(authenticator, all_user_info)
